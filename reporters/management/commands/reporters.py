945514278ab85606b8e8cd6d5b9e30989b232dc1
hdknr <gmail@hdknr.com>
2019-05-13 12:36:24 +0900

    Ignore patterns and check it by command

--- reporters/management/commands/reporters.py
+++ reporters/management/commands/reporters.py
@@ -5,6 +5,7 @@ from reporters import utils
 from bs4 import BeautifulSoup as Soup
 import os
 import re
+import json
 
 
 logger = getLogger()
@@ -12,25 +13,46 @@ translation.activate('ja')
 
 
 @click.group(invoke_without_command=True)
+@click.option('--location', '-l', default=None, 
+              help="parent directory of the conf.BASE(MEDIA_ROOT)")
 @click.pass_context
-def main(ctx):
-    pass
+def main(ctx, location):
+    # print(type(ctx), dir(ctx))
+    ctx.obj = {'location': location}
 
 
-def check_invalid_host_header(value, soup):
-    match = re.search("^Invalid HTTP_HOST header:.+'(?P<host>[^']+)'.", value)
-    return match
-
+IGNORES = [
+    "^Invalid HTTP_HOST header:.+'(?P<host>[^']+)'.",
+    "^\[Errno 2\] No such file or directory:.+'(?P<path>[^']+)'",
+]
 
 @main.command()
-@click.option('--base', '-b', default=None)
+@click.argument('filename')
+@click.option('--delete', '-d', is_flag=True)
 @click.pass_context
-def delete_invalid_host_header(ctx, base):
-    '''List Files'''
-    for path in utils.list_files(location=base):
-        soup = Soup(open(path), "html.parser")
-        exception_value = soup.select('.exception_value')[0].text
-        match = check_invalid_host_header(exception_value, soup)
-        if match:
-            click.echo(match.groupdict()['host'])
+def can_ignore(ctx, filename, delete):
+    '''Check if can be ignored'''
+
+    def _report(path, pattern, ma, do_delete):
+        params = {
+            "path": path,
+            "pattern": pattern,
+            "params": ma.groupdict(),
+        }
+        click.echo(json.dumps(params,indent=2))
+        if do_delete:
             os.remove(path)
+
+    def _do(path):
+        soup = utils.get_soup_from(path)
+        for pattern in IGNORES:
+            ma, text = utils.parse_soup(soup, pattern)
+            if ma:
+                _report(path, pattern, ma, delete)
+
+    if filename == 'all':
+        for path in utils.list_files(location=ctx.obj['location']):
+            _do(path)
+        return
+
+    _do(utils.get_storage_path(filename, location=ctx.obj['location']))
--- reporters/utils.py
+++ reporters/utils.py
@@ -1,7 +1,9 @@
 from django.template import Template, Context, loader
 from django.core.files.storage import FileSystemStorage
+from bs4 import BeautifulSoup as Soup
 import os
 from . import conf
+import re
 
 
 def render(src, request=None, **kwargs):
@@ -13,9 +15,29 @@ def get_storage(*args, **kwargs):
 
 
 def list_files(*args, **kwargs):
-    path = get_storage(*args, **kwargs).path('')
+    path = get_storage(*args, **kwargs).path(conf.BASE)
     for (root, _dirs, files) in os.walk(path):
         for filename in files:
             if not filename.endswith('html'):
                 continue
             yield os.path.join(root, filename)
+
+
+def get_storage_path(filename, *args, **kwargs):
+    filename = os.path.join(conf.BASE, filename)
+    return get_storage(*args, **kwargs).path(filename)
+
+
+def get_soup(html_stream):
+    return Soup(html_stream, "html.parser")
+
+
+def get_soup_from(filename, *args, **kwargs):
+    if not filename.startswith('/'):
+        filename = os.path.join(conf.BASE, filename)
+    return get_soup(open(filename))
+
+
+def parse_soup(soup, pattern, selector='.exception_value'):
+    src = soup.select(selector)[0].text
+    return (re.search(pattern, src), src)
